6b3be168264f2f2365d297fc65a1c563c504e41c
Kevin Ku <kevinku@cmu.edu>
2016-02-07 01:22:59 -0500

    Pad sound with noise breaks

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 # Frequency definitions
 ZERO_FREQUENCY = 500
-ONE_FREQUENCY = 1000
+ONE_FREQUENCY = 5000
 SYN_FREQUENCY = 10000
 
 # Tone window (seconds)
-WINDOW = 1
+WINDOW = 0.1
 
 # wav format
 CHUNK = 1024
--- gen_message.py
+++ gen_message.py
@@ -3,6 +3,7 @@ from config import *
 import pyaudio
 import time
 import wave
+import random
 
 def getFrames(f):
   wf = wave.open(f, 'rb')
@@ -22,6 +23,7 @@ def getFrames(f):
 one = getFrames("one.wav")
 zero = getFrames("zero.wav")
 syn = getFrames("syn.wav")
+noise = getFrames("noise.wav")
 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT/2,
@@ -29,16 +31,33 @@ stream = p.open(format=FORMAT/2,
                 rate=RATE,
                 output=True)
 
+def playNoise():
+  for f in noise:
+    stream.write(f)
+
 for f in syn:
   stream.write(f)
+playNoise()
+# Fill in with blanks
+# restFrames = int(RATE * WINDOW) % RATE
+# stream.write(b'\x80' * restFrames)
 
-for i in xrange(0, 4):
-  if i % 2 == 0:
+for i in xrange(0, 16):
+  b = random.randint(0, 1)
+  print b
+
+  if b == 1:
     for f in one:
       stream.write(f)
   else:
     for f in zero:
       stream.write(f)
+
+  playNoise()
+
+  # Fill in with blanks
+  # restFrames = int(RATE * WINDOW) % RATE
+  # stream.write(b'\x80' * restFrames)
 
 stream.stop_stream()
 stream.close()
